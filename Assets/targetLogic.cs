04aa0ae281428a084d604f379953e895749f6522
AndreCorreaSantos <acorreasantos2002@gmail.com>
2024-03-03 17:47:19 -0300

    adding buzzer sound

--- Assets/targetLogic.cs
+++ Assets/targetLogic.cs
@@ -7,11 +7,16 @@ public class targetLogic : MonoBehaviour
 
     public Transform spawnTransform;
     public GameObject prefab;
- private void OnTriggerEnter(Collider other)
-    {
 
-           // spawn prefab at spawn transform
+    public AudioSource source;
+
+    public AudioClip buzzer;
+    private void OnTriggerEnter(Collider other)
+        {
+
+            // spawn prefab at spawn transform
             Instantiate(prefab, spawnTransform.position, spawnTransform.rotation);
-    }
+            source.PlayOneShot(buzzer,0.5f);
+        }
 
 }
